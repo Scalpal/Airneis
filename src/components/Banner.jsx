4a3d46ea7ed41108b8e36f7a1fa9aab40fdbad57
Scalpal <90528753+Scalpal@users.noreply.github.com>
2023-01-28 15:29:42 +0100

    [ADD] Embla slider system for banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,16 +1,43 @@
 import useEmblaCarousel from "embla-carousel-react";
 import styles from "@/styles/Banner.module.css";
+import Autoplay from "embla-carousel-autoplay";
+import Image from "next/image"; 
 
 const Banner = () => {
 
-  const [emblaRef] = useEmblaCarousel(); 
+  const autoplayOptions = { delay: 5000 }; 
+  const PLUGINS = [Autoplay(autoplayOptions)];
+  const OPTIONS = { loop: true }; 
+
+  // Placeholder images for slider, but the structure of the pictures array will be the same (Pascal)
+  const placeholderImages = [
+    "/meuble-1.jpeg",
+    "/meuble-2.jpeg",
+    "/meuble-3.png",
+  ];
+
+  const [emblaRef] = useEmblaCarousel(OPTIONS, PLUGINS); 
 
   return (
-    <div className={styles.banner} ref={emblaRef}>
+    <header className={styles.banner} ref={emblaRef}>
       <div className={styles.bannerSlidesContainer}>
-
+        
+        {placeholderImages.map((imageSrc, index) => {
+          return (
+            <div key={index} className={styles.bannerSlide}>
+              <Image
+                src={imageSrc}
+                alt={"Carousel Image" + index}
+                fill
+                style={{
+                  objectFit: "cover",
+                }}
+              />
+            </div>
+          );
+        })}
       </div>
-    </div>
+    </header>
   );
 };
 
