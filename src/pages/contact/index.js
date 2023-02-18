efbd874758e5b3e80f402d497d8fca1100f2017b
caarstn <115779362+caarstn@users.noreply.github.com>
2023-02-18 09:22:39 +0100

    Update registration and added contact

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,12 +1,26 @@
-import Banner from "@/components/Banner";
+import Form from "@/components/Form";
+import Navbar from "@/components/Navbar";
+import * as yup from "yup";
 
-export default function Contact () {
+export default function Contact() {
+  const initialValues = { email: "", subject: "", message: "" };
+  const schema = yup.object().shape({
+    email: yup.string().required().email(),
+    subject: yup
+      .string()
+      .required()
+      .max(50, "Your subject must contain 50 characters maximum"),
+    message: yup
+      .string()
+      .required()
+      .max(700, "Your message must contain 700 characters maximum"),
+  });
 
   return (
     <>
-      <Banner />
-      <h2> Contact page </h2>
-
+      <Navbar />
+      <h2>Contact</h2>
+      <Form initialValues={initialValues} schema={schema} />
     </>
   );
 }
