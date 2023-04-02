8dea0bb5e6ce0cb49af5cedf73e7a0c7309221f4
Alexandre Schecht <alexandreschecht@gmail.com>
2023-04-02 22:50:00 +0200

    version with responsive design

--- src/pages/category/[categoryName]/index.jsx
+++ src/pages/category/[categoryName]/index.jsx
@@ -4,90 +4,86 @@ import { useRouter } from "next/router";
 import styles from "@/styles/categoryPage.module.css";
 
 const Category = () => {
+  const router = useRouter();
+  const categoryProducts = [
+    {
+      id: 1,
+      name: "Chaise moderne en bois de hêtre",
+      type: "bois",
+      description:
+        "Chaises noir en bois de hêtre centenaire d'Himalayad,zkdanaldza nzdn lkdn jlkdznland kzalzdnalkd nkldzndzlaknalkn",
+      price: "145$",
+      stock: 25,
+      imageSrc: "/meuble-2.jpeg",
+      materials: ["métal","acier","fer"],
+    },
+    {
+      id: 2,
+      name: "chaise",
+      type: "bois",
+      price: "145$",
+      stock: 25,
+      imageSrc: "/meuble-2.jpeg",
+      materials: ["métal","acier","fer"],
+    },
+    {
+      id: 3,
+      name: "chaise",
+      type: "bois",
+      description: "Chaises noir en bois de hêtre centenaire d'Himalaya",
+      price: "145$",
+      stock: 25,
+      imageSrc: "/meuble-2.jpeg",
+      materials: ["métal","acier","fer"],
+    },
+    {
+      id: 4,
+      name: "chaise",
+      type: "bois",
+      price: "145$",
+      stock: 25,
+      imageSrc: "/meuble-2.jpeg",
+      materials: ["métal","acier","fer"],
+    },
+    {
+      id: 5,
+      name: "chaise",
+      type: "bois",
+      description: "Chaises noir en bois de hêtre centenaire d'Himalaya",
+      price: "145$",
+      stock: 25,
+      imageSrc: "/meuble-2.jpeg",
+      materials: ["métal","acier","fer"],
+    },
+    {
+      id: 6,
+      name: "chaise",
+      type: "bois",
+      price: "145$",
+      stock: 25,
+      imageSrc: "/meuble-2.jpeg",
+      materials: ["métal","acier","fer"],
+    },
+  ];
 
-      const router = useRouter();
-      const categoryProducts = [
-            {
-                  id: 1,
-                  name: "Chaise moderne en bois de hêtre",
-                  type: "bois",
-                  description: "Chaises noir en bois de hêtre centenaire d'Himalayad,zkdanaldza nzdn lkdn jlkdznland kzalzdnalkd nkldzndzlaknalkn",
-                  price: "145$",
-                  stock: 25,
-                  imageSrc: "/meuble-2.jpeg",
-                  materials: ["métal","acier","fer"]
-            },
-            {
-                  id: 2,
-                  name: "chaise",
-                  type: "bois",
-                  price: "145$",
-                  stock: 25,
-                  imageSrc: "/meuble-2.jpeg",
-                  materials: ["métal","acier","fer"]
-            },
-            {
-                  id: 3,
-                  name: "chaise",
-                  type: "bois",
-                  description: "Chaises noir en bois de hêtre centenaire d'Himalaya",
-                  price: "145$",
-                  stock: 25,
-                  imageSrc: "/meuble-2.jpeg",
-                  materials: ["métal","acier","fer"]
-            },
-            {
-                  id: 4,
-                  name: "chaise",
-                  type: "bois",
-                  price: "145$",
-                  stock: 25,
-                  imageSrc: "/meuble-2.jpeg",
-                  materials: ["métal","acier","fer"]
-            },
-            {
-                  id: 5,
-                  name: "chaise",
-                  type: "bois",
-                  description: "Chaises noir en bois de hêtre centenaire d'Himalaya",
-                  price: "145$",
-                  stock: 25,
-                  imageSrc: "/meuble-2.jpeg",
-                  materials: ["métal","acier","fer"]
-            },
-            {
-                  id: 6,
-                  name: "chaise",
-                  type: "bois",
-                  price: "145$",
-                  stock: 25,
-                  imageSrc: "/meuble-2.jpeg",
-                  materials: ["métal","acier","fer"]
-            }
-      ];
+  return (
+    <>
+      <Banner title={router.query.categoryName} />
 
+      <main>
+        <p className={styles.descriptionText}>
+          Nos chaises, fabriquée en bois de chêne sont d’une qualité premium,
+          idéale pour une maison moderne.
+        </p>
 
-      return (
-            <>
-                  <Banner title={router.query.categoryName} />
-
-                  <main>
-                        <p
-                              className={styles.descriptionText}
-                        >
-                              Nos chaises, fabriquée en bois de chêne sont d’une qualité premium, idéale pour une maison moderne.
-                        </p>
-
-                        <div className={styles.productsList}>
-                              {categoryProducts.map((product,index) => {
-                                    return (
-                                          <DetailedProductCard key={index} product={product} />
-                                    );
-                              })}
-                        </div>
-                  </main>
-            </>
-      );
+        <div className={styles.productsList}>
+          {categoryProducts.map((product,index) => {
+            return <DetailedProductCard key={index} product={product} />;
+          })}
+        </div>
+      </main>
+    </>
+  );
 };
 Category.isPublic = true;
 export default Category;
