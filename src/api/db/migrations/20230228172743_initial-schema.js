cd6423e38e8118ab3d3b9d1bb0bc6b1317d05b1b
Scalpal <90528753+Scalpal@users.noreply.github.com>
2023-04-27 23:45:35 +0200

    update: initial schema
    - Restructured properly the knex initial migration

--- src/api/db/migrations/20230228172743_initial-schema.js
+++ src/api/db/migrations/20230228172743_initial-schema.js
@@ -1,70 +1,55 @@
 export const up = async (knex) => {
+  // Related to users
+  await knex.schema.createTable("role", (table) => { 
+    table.increments("id"); 
+    table.enum("role", ["admin", "user"]).notNullable();  
+  });
+
   await knex.schema.createTable("users", (table) => {
     table.increments("id");
     table.text("email").notNullable().unique();
     table.text("passwordHash").notNullable();
     table.text("passwordSalt").notNullable();
-    table.string("firstName").notNullable();
-    table.string("lastName").notNullable();
-    table.string("phoneNumber").notNullable();
-    table.text("address");
-    table.boolean("activate").notNullable().defaultTo(false);
-    table.string("postcode");
-    table.string("city");
-    table.string("region");
-    table.string("contry");
+    table.text("firstName").notNullable();
+    table.text("lastName").notNullable();
+    table.text("phoneNumber").notNullable();
+    table.boolean("active").notNullable().defaultTo(false);
+    table.integer("roleId").notNullable().references("id").inTable("role").defaultTo(2); 
     table.timestamps(true, true, true);
   });
-  await knex.schema.createTable("user_role", (table) => {
+
+  await knex.schema.createTable("adress", (table) => {
     table.increments("id");
-    table.integer("usersId").references("id").inTable("users").notNullable();
-    table
-      .enu("role", ["admin", "utilisateur"])
-      .notNullable()
-      .defaultTo("utilisateur");
-    table.timestamps(true, true, true);
+    table.text("adress");
+    table.text("city");
+    table.text("region");
+    table.text("postalCode");
+    table.text("country");
+    table.bool("mainAdress").notNullable().defaultTo(false); 
+    table.integer("userId").notNullable().references("id").inTable("users");
   });
-  await knex.schema.createTable("category", (table) => {
+
+  // Related to products
+  await knex.schema.createTable("categories", (table) => {
     table.increments("id");
     table.text("name").notNullable();
-    table.timestamps(true, true, true);
   });
+
   await knex.schema.createTable("materials", (table) => {
     table.increments("id");
     table.text("name").notNullable();
-    table.timestamps(true, true, true);
   });
-  await knex.schema.createTable("product", (table) => {
+
+  await knex.schema.createTable("products", (table) => {
     table.increments("id");
-    table.string("title").notNullable();
+    table.text("name").notNullable();
     table.text("description").notNullable();
     table.integer("price").notNullable();
-    table
-      .integer("materialsId")
-      .references("id")
-      .inTable("materials")
-      .notNullable();
-    table.integer("quantity").notNullable().defaultTo(1);
-    table
-      .integer("categoryId")
-      .references("id")
-      .inTable("category")
-      .notNullable();
+    table.integer("stock").notNullable().defaultTo(1);
+    table.integer("categoryId").notNullable().references("id").inTable("categories"); 
     table.timestamps(true, true, true);
   });
-  await knex.schema.createTable("review", (table) => {
-    table.increments("id");
-    table
-      .integer("productId")
-      .references("id")
-      .inTable("product")
-      .notNullable();
-    table.integer("usersId").references("id").inTable("users").notNullable();
-    table.string("title").notNullable();
-    table.text("comment").notNullable();
-    table.double("rate", 2, 1);
-    table.timestamps(true, true, true);
-  });
+
   await knex.schema.createTable("productImage", (table) => {
     table.increments("id");
     table
@@ -75,24 +60,38 @@ export const up = async (knex) => {
     table.binary("image").notNullable();
     table.timestamps(true, true, true);
   });
-  await knex.schema.createTable("command", (table) => {
+
+  await knex.schema.createTable("products_materials_relation", (table) => {
+    table.integer("productId").notNullable().references("id").inTable("products");
+    table.integer("materialId").notNullable().references("id").inTable("materials");
+    table.primary(["productId", "materialId"]); 
+  });
+
+  // Related to reviews
+  await knex.schema.createTable("review", (table) => {
     table.increments("id");
-    table.integer("usersId").references("id").inTable("users").notNullable();
-    table
-      .integer("productId")
-      .references("id")
-      .inTable("product")
-      .notNullable();
-    table.integer("quantity").notNullable().defaultTo(1);
-    table
-      .enu("status", [
-        "en cours de validation",
-        "préparation de la commande",
-        "en cours de livraison",
-        "livré",
-      ])
-      .notNullable();
+    table.integer("productId").references("id").inTable("product").notNullable();
+    table.integer("userId").references("id").inTable("users").notNullable();
+    table.text("title").notNullable();
+    table.text("content").notNullable();
+    table.enum("stars", [1, 2, 3, 4, 5]).notNullable().defaultTo(1); 
     table.timestamps(true, true, true);
+  });
+
+  // Related to orders
+  await knex.schema.createTable("orders", (table) => {
+    table.increments("id");
+    table.integer("userId").notNullable().references("id").inTable("users");
+    table.integer("deliveryAdress").notNullable().references("id").inTable("adress"); 
+    table.enum("status", ["cancelled","delivered"]).notNullable();
+    table.timestamps(true, true, true);
+  });
+
+  await knex.schema.createTable("orders_products_relation", (table) => {
+    table.integer("orderId").notNullable().references("id").inTable("orders");
+    table.integer("productId").notNullable().references("id").inTable("products");
+    table.integer("quantity").notNullable().defaultTo(1); 
+    table.primary(["orderId", "productId"]);
   });
 };
 // a changer : 2 etats : annulé / livré 
