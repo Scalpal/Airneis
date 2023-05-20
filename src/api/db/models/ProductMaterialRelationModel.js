9bbab74be45b418f3a70e5c431859c7f43cbbb3e
Scalpal <90528753+Scalpal@users.noreply.github.com>
2023-05-20 21:04:50 +0200

    update: products and materials model
    - Renamed products and material relation model
    - Updated ProductModel : added materials relation mappings to get all materials related to product

--- src/api/db/models/ProductMaterialRelationModel.js
+++ src/api/db/models/ProductMaterialRelationModel.js
@@ -2,8 +2,13 @@ import BaseModel from "@/api/db/models/BaseModel.js";
 import ProductModel from "@/api/db/models/ProductModel";
 import MaterialModel from "@/api/db/models/MaterialModel";
 
+<<<<<<< Updated upstream:src/api/db/models/ProductMaterialRelation.js
 class ProductMaterialModel extends BaseModel {
   static tableName = "products_materials_relation";
+=======
+class ProductMaterialRelationModel extends BaseModel {
+  static tableName = "products_materials_relation"
+>>>>>>> Stashed changes:src/api/db/models/ProductMaterialRelationModel.js
 
   static relationMappings() {
     return {
@@ -27,4 +32,4 @@ class ProductMaterialModel extends BaseModel {
   }
 }
 
-export default ProductMaterialModel;
+export default ProductMaterialRelationModel;
--- src/api/db/models/ProductModel.js
+++ src/api/db/models/ProductModel.js
@@ -1,5 +1,6 @@
 import BaseModel from "@/api/db/models/BaseModel.js";
 import CategoryModel from "@/api/db/models/CategoryModel.js";
+import MaterialModel from "./MaterialModel";
 
 class ProductModel extends BaseModel {
   static tableName = "products";
@@ -14,6 +15,18 @@ class ProductModel extends BaseModel {
           to: "categories.id",
         },
       },
+      materials: {
+        relation: BaseModel.ManyToManyRelation,
+        modelClass: MaterialModel,
+        join: {
+          from: "products.id",
+          through: {
+            from: "products_materials_relation.productId",
+            to: "products_materials_relation.materialId"
+          },
+          to: "materials.id"
+        }
+      }
     };
   }
 }
