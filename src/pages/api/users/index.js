361d3752bebbf2b4e6ca2489bc45c03074ceb561
Scalpal <90528753+Scalpal@users.noreply.github.com>
2023-05-23 10:10:11 +0200

    update: API users route GET
    - Changed search system, now case insensitive

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -36,6 +36,7 @@ const handler = mw({
       },
       res,
     }) => {
+      const searchValue = search.toLowerCase(); 
       const query = UserModel.query();
 
       if (orderField) {
@@ -44,29 +45,19 @@ const handler = mw({
 
       if (search) {
         query
-          .where("firstName", "like", `%${search}%`)
-          .orWhere("lastName", "like", `%${search}%`)
-          .orWhere("email", "like", `%${search}%`);
+          .whereRaw("LOWER(\"firstName\") LIKE ?", `%${searchValue}%`)
+          .orWhereRaw("LOWER(\"lastName\") LIKE ?", `%${searchValue}%`)
+          .orWhereRaw("LOWER(\"email\") LIKE ?", `%${searchValue}%`);
       }
 
       const countQuery = query.clone();
       const [{ count }] = await countQuery.clearSelect().clearOrder().count();
 
-      const users = await query
-        .modify("paginate", limit, page)
-        .select(
-          "id",
-          "email",
-          "firstName",
-          "lastName",
-          "phoneNumber",
-          "active",
-          "isAdmin"
-        );
-
+      const users = await query.modify("paginate", limit, page)
+        .select("id", "email", "firstName", "lastName", "phoneNumber", "active", "isAdmin");
+      
       res.send({ users: users, count: count });
     },
   ],
 });
-
 export default handler;
