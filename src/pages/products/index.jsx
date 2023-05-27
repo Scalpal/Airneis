605291b15867ad8dc8619326884a28991fee49c8
Alexandre Schecht <alexandreschecht@gmail.com>
2023-05-27 13:50:52 +0200

    fixed number of product display on product page

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -50,10 +50,10 @@ const Products = () => {
 
     setPage(index ? Number.parseInt(index) : 1)
 
-    setStartIndex((index - 1) * limit + 1)
-    const indexEnd =
-      (index - 1) * limit + limit < count ? (index - 1) * limit + limit : count
-    setEndIndex(indexEnd)
+    setEndIndex(
+      (page - 1) * limit + limit < count ? (page - 1) * limit + limit : count
+    )
+    setStartIndex((page - 1) * limit + 1)
   }, [count, getProducts, index, queryParams, limit, page])
 
   const searchStateAction = (value) => {
@@ -76,7 +76,7 @@ const Products = () => {
 
         <div className={styles.indexProducts}>
           <span>
-            {startIndex} - {endIndex} on{" "}
+            {endIndex > 0 ? startIndex : 0} - {endIndex} on{" "}
             {count > 100000 ? `plus de ${count}` : count} products
           </span>
         </div>
