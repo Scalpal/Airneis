59959f6fa9f6244dd07867c68c37b88f51719faf
Scalpal <90528753+Scalpal@users.noreply.github.com>
2023-06-04 12:08:08 +0200

    update: useUser hook -> not revalidating on focus

--- src/web/hooks/useUser.jsx
+++ src/web/hooks/useUser.jsx
@@ -17,7 +17,10 @@ const fetcher = async (url) => {
 };
 
 export const useUser = () => {
-  const { data, error, isLoading } = useSWR(routes.api.users.self(),fetcher);
+  const config = {
+    revalidateOnFocus: false,
+  };
+  const { data, error, isLoading } = useSWR(routes.api.users.self(),fetcher, config);
 
   return {
     data: data,
