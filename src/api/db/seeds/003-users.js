69220ee21dd401717d76b9236fa3f4d495a600b4
Alexandre Gautrot <72071525+Gautrot@users.noreply.github.com>
2023-06-25 11:07:45 +0200

    fixed users seed

--- src/api/db/seeds/003-users.js
+++ src/api/db/seeds/003-users.js
@@ -1,30 +1,81 @@
-import * as yup from "yup";
+import { faker } from "@faker-js/faker";
+import hashPassword from "../hashPassword.js";
 
-const checkIsAdmin = () => {
-  return async (ctx) => {
-    const { res, next, logger, locals } = ctx;
-    const user = locals.user;
+export const seed = async (knex) => {
+  const loop = 100;
 
-    try {
-      if (user.isAdmin === false) {
-        res.status(403).send({ error: "Forbidden" });
+  const users = [];
+  for (let i = 0; i < loop; i++) {
+    const [passwordHash, passwordSalt] = await hashPassword(
+      faker.internet.password()
+    );
+    const user = {
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      email: faker.internet.email(),
+      phoneNumber: faker.phone.number(),
+      passwordHash,
+      passwordSalt
+    };
+    users.push(user);
+  }
+  let userIds = [];
+  await knex("users")
+    .insert(users)
+    .returning("id")
+    .then((ids) => (userIds = ids));
+  userIds = userIds.map((user) => user.id);
 
-        return;
-      }
+  const addresses = [];
+  for (let i = 0; i < loop; i++) {
+    const address = {
+      address: faker.location.streetAddress(),
+      city: faker.location.city(),
+      region: faker.location.state(),
+      postalCode: faker.location.zipCode(),
+      country: faker.location.country(),
+      userId: userIds[i]
+    };
+    addresses.push(address);
+  }
+  let addressIds = [];
+  await knex("addresses")
+    .insert(addresses)
+    .returning("id")
+    .then((ids) => (addressIds = ids));
+  addressIds = addressIds.map((address) => address.id);
 
-      next();
-    } catch (error) {
-      if (error instanceof yup.ValidationError) {
-        res.status(422).send({ error: error.errors });
+  const productIds = await knex("products").pluck("id");
 
-        return;
-      }
-
-      logger.error(error);
-
-      res.status(500).send({ error: "Oops. Something went wrong." });
+  if (productIds.length !== 0) {
+    const reviews = [];
+    for (let i = 0; i < loop; i++) {
+      const randomStars = faker.helpers.arrayElement([1, 2, 3, 4, 5]);
+      const review = {
+        title: faker.lorem.lines(1),
+        content: faker.lorem.text(),
+        stars: randomStars,
+        productId: productIds[i],
+        userId: userIds[i]
+      };
+      reviews.push(review);
     }
-  };
-};
+    await knex("reviews").insert(reviews);
+  }
 
-export default checkIsAdmin;
+  const orders = [];
+  for (let i = 0; i < loop; i++) {
+    const randomStatus = faker.helpers.arrayElement([
+      "cancelled",
+      "on standby",
+      "delivered"
+    ]);
+    const order = {
+      status: randomStatus,
+      deliveryAddress: addressIds[i],
+      userId: userIds[i]
+    };
+    orders.push(order);
+  }
+  await knex("orders").insert(orders);
+};
