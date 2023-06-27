c4a122727a30cb77cb27928834e9dd28d02793da
caarstn <115779362+caarstn@users.noreply.github.com>
2023-06-27 11:16:42 +0200

    translate message contact

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,15 +1,32 @@
 import Banner from "@/web/components/Banner";
+import styles from "@/styles/contact.module.css";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 const Contact = () => {
+  const { t } = useTranslation(["contact"]);
+
   return (
     <>
       <Banner title={"Contact"} />
-      <h2> Contact page </h2>
-
-      <p></p>
+      <div className={styles.message}>
+        <h2>{t("messsageAttente")}</h2>
+      </div>
     </>
   )
 }
 
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        "contact",
+        "footer",
+        "drawerMenu",
+        "navbar",
+      ])),
+    },
+  };
+}
 
 export default Contact;
