e984773e805adad683a19c81db8b431ab32458a0
Scalpal <pppascallim@gmail.com>
2023-07-17 17:09:56 +0200

    update: Online db setup

--- knexfile.js
+++ knexfile.js
@@ -6,11 +6,12 @@ config();
 const knexfile = {
   client: "pg",
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    ssl: { rejectUnauthorized: false },
   },
   migrations: {
     directory: resolve("src/api/db/migrations"),
